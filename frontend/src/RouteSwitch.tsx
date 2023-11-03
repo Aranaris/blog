0adaf0e1f11979eb1193f4ffe19240d7363d6dfa
vinceye <vinceye.cm@gmail.com>
2023-11-03 16:24:25 -0700

    css cleanup and re-organizing nav links/pages

--- frontend/src/RouteSwitch.tsx
+++ frontend/src/RouteSwitch.tsx
@@ -1,19 +1,20 @@
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import App from "./App";
 import logo from "./assets/WLogo_Final.png"
+import Blog from "./components/Blog";
 
 function RouteSwitch() {
     return (
         <BrowserRouter>
             <nav>
                 <Link to="/"><img id="logo" src={logo} alt="logo" /></Link>
-                <Link to="/posts/">Blog</Link>
-                <Link to="/users/">Users</Link>
+                <Link to="/posts/">Archive</Link>
+                <Link to="/users/">About</Link>
             </nav>
             <Routes>
-                <Route path="/" element={<App />}/>
-                <Route path="/users/" element={<App />}/>
+                <Route path="/" element={<Blog />}/>
                 <Route path="/posts/" element={<App />}/>
+                <Route path="/users/" element={<App />}/>
             </Routes>
         </BrowserRouter>
     )
---
+++ frontend/src/components/Blog.tsx
@@ -0,0 +1,11 @@
+import { BlogpostAPI } from "../../apis/BlogpostAPI"
+
+function Blog() {
+    return (
+        <div className="Blog">
+            <header>Blog</header>
+        </div>
+    )
+}
+
+export default Blog
