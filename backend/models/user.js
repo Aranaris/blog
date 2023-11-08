ed51726be694cbed8ba1164f6f4f5a3c6eddae33
vinceye <vinceye.cm@gmail.com>
2023-11-07 18:09:06 -0800

    setting up password structure in user model.

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,16 +1,36 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
 const opts = {'toJSON': {'virtuals': true}};
 const userSchema = new Schema({
 	'username': {'type': String, 'required': true, 'unique': true, 'dropDups': true, 'minLength': 5},
-	'password': {'type': String},
+	'password': {
+		'type': String,
+		'required': true,
+		'trim': true,
+		'minLength': 7,
+		validate(value) {
+			if(value.toLowerCase().includes('password')){
+				throw new console.error('Password cannot be password');
+			}
+		},
+		'default': '',
+	},
 	'firstname': {'type': String, 'required': true},
 	'lastname': {'type': String},
 	'dateofbirth': {'type': Date},
 	'lastaction': {'type': Date, 'default': new Date()},
 	'role': {'type': String, 'required': true, 'default': 'new user'},
 }, opts);
+
+userSchema.pre('save', async function (next) {
+	const user = this;
+	if(user.isModified('password')){
+		user.password = await bcrypt.hash(user.password, 10);
+	}
+	next();
+});
 
 module.exports = mongoose.model('User', userSchema);
